fafa12ced4aca5b96d2fd891872a51ab660836f1
村田健太郎 <muratakentarou@MacBook-Air.local>
2020-07-03 14:55:43 +0900

    demonの完成

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKpRsxyIvpekvZxTvghaBa2E5pP377CsU",
+  authDomain: "chatbot-demo-ae696.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-ae696.firebaseio.com",
+  projectId: "chatbot-demo-ae696",
+  storageBucket: "chatbot-demo-ae696.appspot.com",
+  messagingSenderId: "1012811125411",
+  appId: "1:1012811125411:web:b83ce07c6e8a7a23cd6516",
+  measurementId: "G-M023MK72BZ",
+};
+
+export default firebaseConfig;
